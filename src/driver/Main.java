6d87d7a68c0d67453750692664046c21a09e02cc
krishna Kumar Singh <krishnakumarsingh@users.noreply.github.com>
2022-06-19 13:09:38 +0530

    Submit krishnaKumar-OOPsAssignmentSolution

---
+++ src/driver/Main.java
@@ -0,0 +1,37 @@
+package driver;
+import model.HrDepartment;
+import model.AdminDepartment;
+import model.TechDepartment;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AdminDepartment AdminDeprt = new AdminDepartment();
+		HrDepartment hrDeprt = new HrDepartment();
+		TechDepartment techDeprt = new TechDepartment();
+
+		// For admin Department
+		System.out.println("Welcome to " + AdminDeprt.departmentName());
+		System.out.println(AdminDeprt.getTodaysWork());
+		System.out.println(AdminDeprt.getWorkDeadline());
+		System.out.println(AdminDeprt.isTodayAHoliday());
+
+		// For hr Department
+		System.out.println();
+		System.out.println("Welcome to " + hrDeprt.departmentName());
+		System.out.println(hrDeprt.doActivity());
+		System.out.println(hrDeprt.getTodaysWork());
+		System.out.println(hrDeprt.getWorkDeadline());
+		System.out.println(hrDeprt.isTodayAHoliday());
+		
+		// For tech Department
+		System.out.println();
+		System.out.println("Welcome to " + techDeprt.departmentName());
+		System.out.println(techDeprt.getTodaysWork());
+		System.out.println(techDeprt.getWorkDeadline());
+		System.out.println(techDeprt.getTechStackInformation());
+		System.out.println(techDeprt.isTodayAHoliday());
+
+	}
+
+}
